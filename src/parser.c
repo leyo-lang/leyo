a6290838ebaeac041494e41c94acc3f79b46b58a
Josh Ruddick <124031454+JoshRuds@users.noreply.github.com>
2026-07-06 12:02:41 +0100

    Initialize byte buffer and add overflow check
    Added byte buffer initialization and check for overflow.

--- src/parser.c
+++ src/parser.c
@@ -95,6 +95,7 @@ static void expectAndPass(TokenType type, char *errorStr) {
 }
 
 static void emit(uint8_t value) {
+    checkByteBuff();
     if ((size_t)b->byteIndex >= sizeof(b->bytebuff)) {
         logBuildParser("Byte buffer overflow detected");
         raise("Byte buffer overflow", current().line, current().collumn);
@@ -708,6 +709,8 @@ ByteCodeResult parse(TokenStream *ts) {
     b->tokens = ts->stream;
     b->count = ts->count;
     b->pos = 0;
+    b->byteCap = 64;
+    b->byteBuff = malloc(sizeof(uint8_t) * b->byteCap);
     b->byteIndex = 0;
     b->globalCount = 0;
     b->funcs = malloc(sizeof(Func) * 1024);
@@ -829,6 +832,8 @@ ByteCodeResult parse(TokenStream *ts) {
     cb.length = amount;
     memcpy(cb.data, constsFinal, cb.length);
     res.cb = cb;
+
+    free(b->byteBuff);
     
     return res;
 }
